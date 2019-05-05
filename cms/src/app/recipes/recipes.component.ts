6e5ae1b1f292478832f2d79335a390febd57efd6
Bryan Crowder <3107024@gmail.com>
2019-05-04 21:56:37 -0600

    Section 3
    Setting up the website!!

---
+++ cms/src/app/recipes/recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
