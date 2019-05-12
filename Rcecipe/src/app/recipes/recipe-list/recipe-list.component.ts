72a06801625d39fd772cabe25b74b3bd2f56c035
Bryan Crowder <3107024@gmail.com>
2019-05-11 23:26:34 -0600

    week 3!

--- Rcecipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Rcecipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 
@@ -8,12 +8,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'this is simple a test', "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/11/WU2001_Cast-Iron-Pizza_s4x3.jpg.rend.hgtvcom.826.620.suffix/1536690646007.jpeg")
   ];
     
   constructor() { }
-
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
   ngOnInit() {
   }
 
--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -2,14 +2,15 @@ export class Contact {
     public contactId: number;
     public name: string;
     public email: string;
-    public phone: number;
+    public phone: string;
     public imageUrl: string;
     public group: string;
 
-    constructor(contactId: number, name: string, email: string, phone: number, imageUrl: string, group: string) {
+    constructor(contactId: number, name: string, email: string, phone: string, imageUrl: string, group: string) {
         this.contactId = contactId;       
         this.name = name;
         this.email = email;
+        this.phone = phone;
         this.imageUrl = imageUrl;
         this.group = group;
     }
