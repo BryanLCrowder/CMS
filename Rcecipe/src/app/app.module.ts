401fb1c39e46ec2f927beb879aa43851ba5a5f00
BryanLCrowder <47199671+BryanLCrowder@users.noreply.github.com>
2019-05-14 09:15:52 -0600

    going to class

--- Rcecipe/src/app/app.module.ts
+++ Rcecipe/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdowDirective } from './shared/dropdown.directive';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -22,7 +22,7 @@ import { DropdowDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent, 
-    DropdowDirective
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
