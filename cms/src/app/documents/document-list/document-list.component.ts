2671fc353a2bacdb0e6e3e1b1686e530dec6d966
Bryan Crowder <3107024@gmail.com>
2019-05-17 12:22:53 -0600

    submit

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -9,8 +9,8 @@ import { Document } from '../document.model';
 export class DocumentListComponent implements OnInit {
   @Output() documentWasSelected = new EventEmitter<Document>();
   documents: Document[] =[
-    new Document(1, "CIT366", "This class has taught me a lot about the MEAN stack.", "byui.edu", "none")
-     ]
+    new Document(1, "CIT366", " This class has taught me a lot about the MEAN stack.", "byui.edu", "none")
+,   new Document(1, "CIT365", " Do the Megadesk project.", "byui.edu", "none")     ]
   constructor() { }
   onDocumentSelected(document: Document){
     this.documentWasSelected.emit(document);
